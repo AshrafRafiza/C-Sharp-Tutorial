eed14ea23df0dcc13fb0daed4eeabe89004069c8
Ashraf Rafiza <ashrafrafiza96@gmail.com>
2022-06-15 15:02:50 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("There once was a man named George");
+            Console.WriteLine("He was 70 years old");
+            Console.WriteLine("He really liked the name George");
+            Console.WriteLine("But didn't like being 70");
+
+            Console.ReadLine();
+        }
+    }
+}
